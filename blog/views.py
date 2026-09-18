f3f6e5c8787abb7e8095b699229e38b2a2b38c6b
DevR4z <rf00101@gmail.com>
2026-09-17 21:30:09 -0300

    Pausa para aprender SQL, Linux, HTTP e API

--- blog/views.py
+++ blog/views.py
@@ -2,14 +2,24 @@ from django.shortcuts import render
 
 
 def blog(request):
+    context = {
+            'text': 'Blog Page'
+        }
+
     return render(
         request,
-        'blog/blog_home.html'
+        'blog/blog_home.html',
+        context,
         )
 
 
 def exemplo(request):
+    context = {
+            'text': 'Exemplo Page'
+        }
+
     return render(
         request,
-        'blog/exemplo.html'
+        'blog/exemplo.html',
+        context
         )
--- home/views.py
+++ home/views.py
@@ -3,7 +3,13 @@ from django.shortcuts import render
 
 def home(request):
     print('Home')
+
+    context = {
+        'text': 'Home Page'
+    }
+
     return render(
         request,
-        'home/index.html'
+        'home/index.html',
+        context,
     )
